91e4287762aaecc3ff12c3d581829b8ae5ec8ec6
RaMandeep2002 <ramandeepsingh1511@gmail.com>
2024-04-30 15:17:57 +0530

    some changes in index and env file

--- index.js
+++ index.js
@@ -3,43 +3,45 @@ require('dotenv').config();
 const contractabi = require('./contractabi.json');
 const tokencontractabi = require('./tokenContractabi.json');
 
+// Provider URL from environment variables
+const providerUrl = `${process.env.API_KEY}`;
+
+// Contract address and token contract address
+const contractAddress = '0xac4b76d96cd3074db649d2c6de9fce973152da6d';
+const tokencontractaddress = '0x63F7a20eF2C7E5A77A578Ca97762cdd1fE710589';
+
+// Amount of tokens to approve (in smallest units)
+const bitamount = 100;
+
+const walletOne = '0xfD2E0FBe4A8a871702F464D7dB888bb3814f4A93';
+const walletTwo = '0xFa7E807a6084579cecA61Dc44678EC03627199d4';
+
+// Create a new Web3 instance with the provider URL
+const web3 = new Web3(providerUrl);
+
+// Create instances of the contract and token contract
+const contract = new web3.eth.Contract(contractabi, contractAddress);
+const tokencontract = new web3.eth.Contract(
+  tokencontractabi,
+  tokencontractaddress
+);
+web3.eth.accounts.wallet.add(`0x${process.env.PRIVATE_KEY}`);
+var gasPrice = web3.eth.getGasPrice();
 /**
  * Function to approve token
  */
 const tokenapprove = async () => {
-  // Provider URL from environment variables
-  const providerUrl = `${process.env.API_KEY}`;
-
-  // Contract address and token contract address
-  const contractAddress = '0xac4b76d96cd3074db649d2c6de9fce973152da6d';
-  const tokencontractaddress = '0x63F7a20eF2C7E5A77A578Ca97762cdd1fE710589';
-
-  // Amount of tokens to approve (in smallest units)
-  const bitamount = 100;
-
-  const walletOne = '0xaC7162fA7F60f1463594bD704d922aeD05C2702B';
-  const walletTwo = '0x313905A1CB9C1D9fa190e913B00Ffe00Ed17639C';
-
-  // Create a new Web3 instance with the provider URL
-  const web3 = new Web3(providerUrl);
-
-  // Create instances of the contract and token contract
-  const contract = new web3.eth.Contract(contractabi, contractAddress);
-  const tokencontract = new web3.eth.Contract(
-    tokencontractabi,
-    tokencontractaddress
-  );
-  web3.eth.accounts.wallet.add(`0x${process.env.PRIVATE_KEY}`);
-  var gasPrice = await web3.eth.getGasPrice();
   //   console.log('gas Price ==> ', gasPrice);
+  console.log('tokenapprove log1');
+  // return
   try {
     // Wallet addresses
-    const account = '0xb62962Bb2661BC5c5fFfa648A2C2B7A3e8CD4d4B';
-
+    const account = 'add you address';
+    const useraccount = 'add user account';
     // Send approval transaction
-    tokencontract.methods
+    await tokencontract.methods
       .approve(contractAddress, bitamount)
-      .send({ from: account, gasLimit: 190785, gasPrice: 1000000000 })
+      .send({ from: account })
       .on('transactionHash', (hash) => {
         console.log('Approval Transaction Hash:', hash);
       })
@@ -53,5 +55,43 @@ const tokenapprove = async () => {
     console.log('Error ===> ', err);
   }
 };
+
+const onStartBit = async () => {
+  //   console.log('gas Price ==> ', gasPrice);
+  console.log('onStartBit log2');
+  // return
+  try {
+    // Wallet addresses
+    const account = 'add you address';
+    const useraccount = 'add user account';
+    // Send approval transaction
+    await contract.methods
+      .onStartBit(useraccount, bitamount, walletOne, walletTwo)
+      .send({ from: account })
+      .on('transactionHash', (hash) => {
+        console.log('onStartBit Transaction Hash:', hash);
+      })
+      .on('receipt', (receipt) => {
+        console.log('onStartBit Receipt:', receipt);
+      })
+      .on('error', (error) => {
+        console.error('onStartBit Error:', error);
+      });
+  } catch (err) {
+    console.log('Error ===> ', err);
+  }
+};
 // Call the tokenapprove function
-tokenapprove();
+
+async function main() {
+  await tokenapprove();
+  // setTimeout(() => {
+  await onStartBit();
+  // }, 5000);
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
